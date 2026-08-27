55e5189fe91444cea2123719c88f24fba6a06afc
JDUB799 <jaydeehumphrey@gmail.com>
2026-08-27 17:00:12 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# twrp_moto_g_power_5g" 
---
+++ device/motorola/cancunn/omni_cancunn.mk
@@ -0,0 +1,9 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+PRODUCT_DEVICE := cancunn
+PRODUCT_NAME := omni_cancunn
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := cancunn
+PRODUCT_MANUFACTURER := motorola
